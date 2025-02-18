3765d0bdcc760621204e6d884c0627c34d80ae4c
Mathbkj <matheusgblasel@hotmail.com>
2025-02-18 00:07:31 -0300

    Aula1-Finalizada

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,11 @@
-import type { ComponentProps, PropsWithChildren } from "react";
-interface ButtonProps extends PropsWithChildren,ComponentProps<'button'> {}
+import type { ComponentProps, PropsWithChildren } from 'react'
+interface ButtonProps extends PropsWithChildren, ComponentProps<'button'> {}
 
-export function Button(props:ButtonProps) {
-  return (
-    <button {...props} className="px-5 flex justify-between items-center h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"/>
-  );
+export function Button(props: ButtonProps) {
+	return (
+		<button
+			{...props}
+			className="px-5 flex justify-between items-center h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+		/>
+	)
 }
